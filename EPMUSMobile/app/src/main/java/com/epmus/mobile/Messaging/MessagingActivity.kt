c743273a0235d2e9ed21d83950d854ad7c5fbf9f
sampain <pais2301@usherbrooke.ca>
2020-11-14 16:51:02 -0500

    Add toolbar to MessagingActivity

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/Messaging/MessagingActivity.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/Messaging/MessagingActivity.kt
@@ -1,13 +1,50 @@
 package com.epmus.mobile.Messaging
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.epmus.mobile.R
+import com.epmus.mobile.SettingsActivity
+import com.epmus.mobile.ui.login.LoginActivity
+import com.epmus.mobile.ui.login.realmApp
 
 class MessagingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_messaging)
+
+        // toolbar support
+        setSupportActionBar(findViewById(R.id.toolbar_Messaging))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_actions, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            true
+        }
+
+        R.id.action_logout -> {
+            realmApp.currentUser()?.logOutAsync {
+                if (it.isSuccess) {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 }
