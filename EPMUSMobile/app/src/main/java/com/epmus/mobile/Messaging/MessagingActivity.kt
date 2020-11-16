b141b20791c9c40a4ddc9f627dc9aeacdaa3e958
Aida Slimani <ayouda.1986@hotmail.fr>
2020-11-16 14:27:22 -0500

    Temporaire Test1

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/Messaging/MessagingActivity.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/Messaging/MessagingActivity.kt
@@ -1,14 +1,30 @@
 package com.epmus.mobile.Messaging
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.epmus.mobile.R
 import com.epmus.mobile.SettingsActivity
 import com.epmus.mobile.ui.login.LoginActivity
 import com.epmus.mobile.ui.login.realmApp
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import io.realm.mongodb.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_messaging.*
+import kotlinx.android.synthetic.main.activity_new_message.*
+import kotlinx.android.synthetic.main.user_row_message.view.*
 
 class MessagingActivity : AppCompatActivity() {
 
@@ -19,6 +35,31 @@ class MessagingActivity : AppCompatActivity() {
         // toolbar support
         setSupportActionBar(findViewById(R.id.toolbar_Messaging))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+
+                p0.children.forEach {
+                    //Log.d(TAG, it.toString())
+                    val user = it.getValue(User::class.java)
+                    if (user != null){
+                        adapter.add(UserItem(user))
+                    }
+
+                }
+                recyclerView_newMessage.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -46,5 +87,17 @@ class MessagingActivity : AppCompatActivity() {
         else -> {
             super.onOptionsItemSelected(item)
         }
+    }
+}
+
+class UserItem(val user: User, val context: Context) : Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int){
+        viewHolder.itemView.username_textview_new_message.text = user.username
+
+       // Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_message
     }
 }
