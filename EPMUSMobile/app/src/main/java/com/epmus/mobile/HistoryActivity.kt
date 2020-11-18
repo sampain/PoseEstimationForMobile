329e04a7ac75bdec92909a6b6f024c63fba5d7cc
sampain <pais2301@usherbrooke.ca>
2020-11-18 11:45:49 -0500

    Cleanup

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/HistoryActivity.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/HistoryActivity.kt
@@ -1,6 +1,5 @@
 package com.epmus.mobile
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,12 +8,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
-import com.epmus.mobile.program.*
 import java.text.SimpleDateFormat
-import java.util.*
+
+var historyView: RecyclerView? = null
 
 class HistoryActivity : AppCompatActivity() {
-    lateinit var historyList: ArrayList<HistoryData>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,41 +22,19 @@ class HistoryActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        //val findIterable : FindIterable<Document>? = mongoCollection?.find()
-
-        /*findIterable?.iterator()?.getAsync {
-            if (it.isSuccess) {
-                historyList = ArrayList()
-                it.get().forEach {
-                    val historyData = HistoryData(it["name"].toString(), SimpleDateFormat(it["date"].toString()), it["time"].toString() )
-                    historyList.add(historyData)
-                }
-                setupRecyclerView(findViewById(R.id.history_list))
-            } else {
-            }
-        }*/
+        historyView = findViewById(R.id.history_list)
+        setupRecyclerView(findViewById(R.id.history_list))
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = SimpleItemRecyclerViewAdapter(historyList)
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(statistics)
     }
 
     class SimpleItemRecyclerViewAdapter(
-        private val values: List<HistoryData>,
+        private val values: List<Statistics>,
     ) :
         RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
 
-        private val onClickListenerDetails: View.OnClickListener
-
-        init {
-            onClickListenerDetails = View.OnClickListener { v ->
-                val item = v.tag as ProgramContent.ProgramItem
-                val intent = Intent(v.context, ProgramDetailActivity::class.java).apply {
-                    putExtra(ProgramDetailFragment.ARG_ITEM_ID, item.id)
-                }
-                v.context.startActivity(intent)
-            }
-        }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val view = LayoutInflater.from(parent.context)
@@ -73,7 +49,7 @@ class HistoryActivity : AppCompatActivity() {
 
             with(holder.itemView) {
                 tag = item
-                setOnClickListener(onClickListenerDetails)
+                //setOnClickListener(onClickListenerDetails)
             }
         }
 
