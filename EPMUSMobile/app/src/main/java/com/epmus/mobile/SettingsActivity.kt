8ee019e2b540bd27c5ff12d2c40853f078f759ef
sampain <pais2301@usherbrooke.ca>
2020-11-28 20:54:12 -0500

    Add toolbar for settings

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/SettingsActivity.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.epmus.mobile
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,8 +15,19 @@ class SettingsActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.settings, SettingsFragment())
             .commit()
+        val toolbar = findViewById<Toolbar>(R.id.settings_toolbar)
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
