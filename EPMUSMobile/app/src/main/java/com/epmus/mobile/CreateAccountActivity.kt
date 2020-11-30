78ce96857eedd6eee1255f4960369a7bd247f670
sampain <pais2301@usherbrooke.ca>
2020-11-30 15:13:48 -0500

    Update create account to force email

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/CreateAccountActivity.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/CreateAccountActivity.kt
@@ -1,20 +1,29 @@
 package com.epmus.mobile
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.widget.Toolbar
-import com.epmus.mobile.ui.login.realmApp
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.epmus.mobile.ui.login.*
 import io.realm.mongodb.Credentials
 import io.realm.mongodb.mongo.MongoClient
 import io.realm.mongodb.mongo.MongoCollection
 import io.realm.mongodb.mongo.MongoDatabase
+import kotlinx.android.synthetic.main.activity_create_account.*
 import org.bson.Document
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +35,75 @@ class CreateAccountActivity : AppCompatActivity() {
 
         val username = findViewById<EditText>(R.id.create_username)
         val password = findViewById<EditText>(R.id.create_password)
-        val createAccoutButton = findViewById<Button>(R.id.createAccoutButton)
+        val createAccoutButton = findViewById<Button>(R.id.createAccountButton)
+        val createAccoutButtonDisabled = findViewById<Button>(R.id.createAccountButtonDisabled)
 
-        createAccoutButton.setOnClickListener {
-            createUser(username.text.toString(), password.text.toString())
+        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+            .get(LoginViewModel::class.java)
+
+        loginViewModel.loginFormState.observe(this, Observer {
+            val loginState = it ?: return@Observer
+
+            // disable login button unless both username / password is valid
+            createAccoutButton.isEnabled = loginState.isDataValid
+            if (loginState.isDataValid) {
+                createAccoutButtonDisabled.visibility = View.GONE
+                createAccoutButton.visibility = View.VISIBLE
+            } else {
+                createAccoutButtonDisabled.visibility = View.VISIBLE
+                createAccoutButton.visibility = View.GONE
+            }
+
+            if (loginState.usernameError != null) {
+                username.error = getString(loginState.usernameError)
+            }
+            if (loginState.passwordError != null) {
+                password.error = getString(loginState.passwordError)
+            }
+        })
+
+        loginViewModel.loginResult.observe(this, Observer {
+            val loginResult = it ?: return@Observer
+
+            loadingCreate.visibility = View.GONE
+            if (loginResult.error != null) {
+                showLoginFailed(loginResult.error)
+            }
+            setResult(Activity.RESULT_OK)
+        })
+
+        username.afterTextChanged {
+            loginViewModel.loginDataChanged(
+                username.text.toString(),
+                password.text.toString()
+            )
         }
+
+        password.apply {
+            afterTextChanged {
+                loginViewModel.loginDataChanged(
+                    username.text.toString(),
+                    password.text.toString()
+                )
+            }
+
+            setOnEditorActionListener { _, actionId, _ ->
+                when (actionId) {
+                    EditorInfo.IME_ACTION_DONE ->
+                        createUser(username.text.toString(), password.text.toString())
+                }
+                false
+            }
+
+            createAccoutButton.setOnClickListener {
+                loading.visibility = View.VISIBLE
+                createUser(username.text.toString(), password.text.toString())
+            }
+        }
+    }
+
+    private fun showLoginFailed(@StringRes errorString: Int) {
+        Toast.makeText(applicationContext, errorString, Toast.LENGTH_SHORT).show()
     }
 
     private fun createUser(username: String, password: String) {
